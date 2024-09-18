5edd2ad33c786859c79854fce88427595176fa17
jennaL808 <lijenna912@gmail.com>
2024-09-17 22:05:23 -0400

    Double Jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 130
 const JUMP_VELOCITY = -300.0
+#the number of jumps 
+var jump_count = 0
+var max_jumps = 2
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -10,11 +13,13 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if is_on_floor():
+		jump_count = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count +=1
+		
 	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
